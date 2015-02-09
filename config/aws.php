7409d0b64141dc45e66ad6a222eb9f1aeaefa77c
Yair Silbermintz <yair@silbermintz.com>
2015-02-09 11:48:16 -0500

    config tweaks

--- config/aws.php
+++ config/aws.php
@@ -5,6 +5,15 @@ return array(
     'config' => array(
         'default_cache_config' => APPPATH.'cache/aws'
     ),
+    
+    /* for development/use on non AWS servers, replace the config above with a key/secret like so:
+    'config' => array(
+        'key' => 'foo',
+        'secret' => 'bar',
+    ),
+    */
+    
+    'region' => 'us-east-1',
 );
 
 ?>          
